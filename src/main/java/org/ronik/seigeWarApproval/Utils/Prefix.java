41d9cfff2819cbcb382e743f95d89fd8e825e403
ronik <ronik.jagdev@gmail.com>
2025-03-06 12:16:40 -0500

    Added global [SiegeWarApproval] prefix.

---
+++ src/main/java/org/ronik/seigeWarApproval/Utils/Prefix.java
@@ -0,0 +1,7 @@
+package org.ronik.seigeWarApproval.Utils;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Prefix {
+    public static final String PREFIX = "[" + ChatColor.GREEN + "SiegeWarApproval" + ChatColor.RESET + "] ";
+}
