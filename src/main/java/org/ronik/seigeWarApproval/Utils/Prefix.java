108d00afa76c5886a274c3b1826c969df3a23a7c
ronik <ronik.jagdev@gmail.com>
2025-03-07 11:03:43 -0500

    Added ronik art for startup

--- src/main/java/org/ronik/seigeWarApproval/Utils/Prefix.java
+++ src/main/java/org/ronik/seigeWarApproval/Utils/Prefix.java
@@ -4,4 +4,11 @@ import net.md_5.bungee.api.ChatColor;
 
 public class Prefix {
     public static final String PREFIX = "[" + ChatColor.GREEN + "SiegeWarApproval" + ChatColor.RESET + "] ";
+    public static final String RONIK_ART = "\n" +
+            "__________              .__ __        ____.\n" +
+            "\\______   \\ ____   ____ |__|  | __   |    |\n" +
+            " |       _//  _ \\ /    \\|  |  |/ /   |    |\n" +
+            " |    |   (  <_> )   |  \\  |    </\\__|    |\n" +
+            " |____|_  /\\____/|___|  /__|__|_ \\________|\n" +
+            "        \\/            \\/        \\/         \n";
 }
